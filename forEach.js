ec25b84c4545e692d168e9ca93d1850a825b57b5
Alexey <mandzhievav@gmail.com>
2017-04-09 10:39:37 +0300

    forEach.js 09/04/2017

--- forEach.js
+++ forEach.js
@@ -1,15 +1,15 @@
 var methods = ['map', 'forEach', 'filter', 'reduce'];
 
-function message(arr) {
-  for (var i = 0; i < arr.length; i++) {
+function message(arr, i) {
     console.log('Это метод ' + arr[i]);
-  }
 }
 
 function forEachFunc(arr, func) {
   
   if (Array.isArray(arr) && (typeof func === 'function')) {
-    func(arr);
+    for (var i = 0; i < arr.length; i++) {
+      func(arr, i);
+    }
   }
   else {
     console.log('Не подходящий тип аргументов');
