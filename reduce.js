edf9de3b36b2aa5f63f044f76d274e0eac6f5f75
Alexey <mandzhievav@gmail.com>
2017-04-11 17:40:49 +0300

    learnJS

--- reduce.js
+++ reduce.js
@@ -1,26 +1,23 @@
 var methods = ['map', 'forEach', 'filter', 'reduce'];
 
 function funcSum(previousValue, currentItem, index, arr) {
-    return previousValue + currentItem;
+  return previousValue + currentItem;
 };
 
 function reduceFunc(arr, func, previousValue) {
   if (Array.isArray(arr) && (typeof func === 'function')) {
     if (previousValue === undefined) {
       previousValue = arr[0];
-      arrayCopy = arr.slice(1);
-      for (var i = 0; i < arrayCopy.length; i++) { 
-        previousValue = func(previousValue, arrayCopy[i], i , arrayCopy);
-      }
-      return previousValue;
+      var arrayCopy = arr.slice(1);
     }
     else {
-      arrayCopy = arr.slice();
-      for (var i = 0; i < arrayCopy.length; i++) { 
-        previousValue = func(previousValue, arrayCopy[i], i , arrayCopy);
-      }
-      return previousValue;
+      var arrayCopy = arr.slice();
     }
+      
+    for (var i = 0; i < arrayCopy.length; i++) { 
+      previousValue = func(previousValue, arrayCopy[i], i , arrayCopy);
+    }
+    return previousValue;
   }
     console.log('Не подходящий тип аргументов');
 }
@@ -28,3 +25,4 @@ function reduceFunc(arr, func, previousValue) {
 console.log(reduceFunc(methods, funcSum));
 var newSum = methods.reduce(funcSum);
 console.log(newSum);
+
